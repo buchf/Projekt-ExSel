7df2b00f5bf2f477fd2e1ac8ba99f5dbb80a9158
buchfelix <buchfelix97@web.de>
2022-07-18 19:31:28 +0200

    Fixed a Text Bug

--- Assets/Scenes/Flexibility/Scripts/WCST_Practice.cs
+++ Assets/Scenes/Flexibility/Scripts/WCST_Practice.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-
+using TMPro;
 
 public class WCST_Practice : MonoBehaviour
 {
 
-    public TextMesh text;
+    [SerializeField] private TextMeshProUGUI text;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        text.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
