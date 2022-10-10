c172c7a445287d9a00b91f55786ff62267931fe5
buchfelix <buchfelix97@web.de>
2022-10-10 17:56:18 +0200

    Implement Corsi and GoNoGo + fixed GoNoGo Bug

---
+++ Assets/Scenes/Arbeitsgedaechtnis/Scripts/SceneSwitch.cs
@@ -0,0 +1,140 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+
+    /*
+     * Easy Scene Switcher index = 0 is the intro, 1 = the play scene, 2 = Outroscene
+     * 
+     */
+
+    private string inputVPN = "";
+    public static bool reverse = false;
+    public static bool reverseVS = true;
+
+    private void Start()
+    {
+        reverseVS = false;
+    }
+    public void StartGame()
+    {
+        DataSaver.z0.Clear();
+        DataSaver.z1.Clear();
+        DataSaver.z2.Clear();
+        DataSaver.z3.Clear();
+        DataSaver.z4.Clear();
+        DataSaver.z5.Clear();
+        DataSaver.z6.Clear();
+        DataSaver.results.Clear();
+        Randomizer.countFalseTask = 0;
+        Randomizer.totalTasks = 0;
+        DataSaver.VPN = inputVPN;
+        Randomizer.reverse = reverse;
+        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 19);
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void BackStart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 126);
+    }
+    public void GoNoGoBackStart()
+    {
+        //GoNoGo.counter = 0;
+        //GoNoGo.trial = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 131);
+    }
+
+    public void ReadInput(string s)
+    {
+        inputVPN = s;
+    }
+
+    public void SetReverse()
+    {
+        reverse = true;
+    }
+
+    public void StartGoNoGO()
+    {
+        
+        DataGoNoGO.overall.Clear();
+        DataGoNoGO.results.Clear();
+        DataGoNoGO.header.Clear();
+        DataGoNoGO.z1.Clear();
+        DataGoNoGO.VPN = inputVPN;
+        
+         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 23);
+    }
+    public void StartCardSorting()
+    {
+        CSPlay.correctResponse = 0;
+        CSDataSaver.header.Clear();
+        CSDataSaver.overall.Clear();
+        CSDataSaver.results.Clear();
+        CSDataSaver.practice.Clear();
+        CSDataSaver.test.Clear();
+        CSDataSaver.VPN = inputVPN;
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 154);
+        //Debug.Log(CSDataSaver.fileName.ToString());
+    }
+
+    public void BackCardSorting()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 159);
+    }
+
+    public void StartWCST()
+    {
+        WCST_Data.header.Clear();
+        WCST_Data.practice.Clear();
+        WCST_Data.test.Clear();
+        WCST_Data.results.Clear();
+        WCST_Data.VPN = inputVPN;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 16);
+    }
+
+    public void StartAX_CPT()
+    {
+        AX_Data.header.Clear();
+        AX_Data.practice.Clear();
+        AX_Data.test.Clear();
+        AX_Data.overall.Clear();
+        AX_Data.results.Clear();
+        AX_Data.VPN = inputVPN;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 20);
+    }
+
+    public void SetVSbackwards()
+    {
+        reverseVS = true;
+    }
+    public void StartVS()
+    {
+        VSData.header.Clear();
+        VSData.practiceOne.Clear();
+        VSData.practiceTwo.Clear();
+        VSData.testOne.Clear();
+        VSData.testTwo.Clear();
+        VSData.testThree.Clear();
+        VSData.testFour.Clear();
+        VSData.testFive.Clear();
+        VSData.testSix.Clear();
+        VSData.testSeven.Clear();
+        VSData.testEigth.Clear();
+        VSData.results.Clear();
+        VSData.totalTask.Clear();
+        VSData.VPN = inputVPN;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 24);
+    }
+
+}
---
+++ Assets/Scenes/Inhibition/GoNoGo/Scripts/GoNoGoPractice.cs
@@ -0,0 +1,262 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GoNoGoPractice : MonoBehaviour
+{
+
+    public AudioSource GoNoGo_02;
+    public AudioSource audiobuff;
+    public int buff = 0;
+    public int buff2 = 0;
+    public GameObject pig;
+    public GameObject chicken;
+    public GameObject cow;
+    public GameObject donkey;
+    [SerializeField] Button button;
+    GameObject shownAnimal;
+    GameObject currentAnimal;
+
+    public TextMesh introText;
+    public TextMesh introText2;
+    public GameObject introAnimal;
+    public GameObject introAnimal2;
+    public Button introButton;
+
+    public Button continueButton;
+    //public Button redoButton;
+    public TextMesh continueText;
+
+    public static Stopwatch timer = new Stopwatch();
+    public int counter;
+    public int trial;
+    public int practiceerrors = 0;
+    public int practicehits = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer.Reset();
+        timer.Stop();
+        introButton.interactable = false;
+        GoNoGo_02.Play();
+        practiceerrors = 0;
+        practicehits = 0;
+        counter = 0;
+        shownAnimal = cow;
+        buff = 0;
+        buff2 = 0;
+        trial = 0;
+        
+    }
+
+    private void Update()
+    {
+
+        if (!audiobuff.isPlaying && buff2 == 1)
+        {
+            continueButton.interactable = true;
+            buff2++;
+        }
+
+        if (counter == 11)
+        {
+            chicken.SetActive(false);
+        }
+        if (!GoNoGo_02.isPlaying && buff == 0)
+        {
+            buff++;
+            introButton.interactable = true;
+        }
+        if (!GoNoGo_02.isPlaying && buff == 2)
+        {
+            buff++;
+            introButton.interactable = true;
+        }
+       
+
+        if (counter == 11 && trial == 2 && buff2 == 0)
+        {
+            if (practiceerrors >= 2 || practicehits < 5)
+            {
+                timer.Reset();
+                RepeatPractice();
+            }
+            else
+            {
+                audiobuff.Play();
+                buff2 = 1;
+                timer.Stop();
+                shownAnimal.gameObject.SetActive(false);
+                chicken.gameObject.SetActive(false);
+                button.gameObject.SetActive(false);
+                continueText.gameObject.SetActive(true);
+                continueButton.gameObject.SetActive(true);
+                continueButton.interactable = false;
+                //redoButton.gameObject.SetActive(true);
+            }
+        }
+
+       
+
+        if (counter == 11 && trial != 2 && !GoNoGo_02.isPlaying && buff == 1)
+        {
+            counter = 1;
+            trial++;
+            //       timer.Stop();
+            //       enableIntro();
+            //       buff = 2;
+        }
+
+
+
+        if (timer.Elapsed.TotalSeconds >= 0.5 && counter != 11)
+        {
+            timer.Reset();
+            //if(counter == 11)
+            // {
+            //  timer.Stop();
+            //  }
+            //  else
+            // {
+            SelectNextAnimal();
+            // }
+
+        }
+    }
+    void enableIntro()
+    {
+        timer.Stop();
+        timer.Reset();
+        GoNoGo_02.Play();
+        introAnimal2.SetActive(true);
+        introText2.gameObject.SetActive(true);
+        introButton.gameObject.SetActive(true);
+        introButton.interactable = false;
+        shownAnimal.gameObject.SetActive(false);
+        button.gameObject.SetActive(false);
+
+    }
+
+    void disableIntro()
+    {
+        trial++;
+        introAnimal.SetActive(false);
+        introText.gameObject.SetActive(false);
+        introAnimal2.SetActive(false);
+        introText2.gameObject.SetActive(false);
+        introButton.gameObject.SetActive(false);
+        button.gameObject.SetActive(true);
+    }
+
+
+
+
+    public void startSequenz()
+    {
+        counter = 1;
+        disableIntro();
+        shownAnimal = donkey;
+        donkey.SetActive(true);
+        button.interactable = true;
+        button.transition = Selectable.Transition.ColorTint;
+        //selectAnimal(counter);
+
+        timer.Start();
+    }
+
+
+
+    public void StartGoNoGo()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void RepeatPractice()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 0);
+    }
+
+    void selectAnimal(int counter)
+    {
+        switch (counter)
+        {
+            case int n when ((counter % 4) == 0):
+                StartCoroutine(showPig());
+                break;
+            case int n when ((counter % 3) == 0):
+                StartCoroutine(showCow());
+                break;
+            case int n when ((counter % 2) == 0):
+                StartCoroutine(showChicken());
+                break;
+            case int n when ((counter % 1) == 0):
+                StartCoroutine(showDonkey());
+                break;
+        }
+    }
+
+    public void clickButton()
+    {
+        if (shownAnimal == cow)
+            practiceerrors++;
+        else
+            practicehits++;
+        timer.Reset();
+        SelectNextAnimal();
+
+    }
+
+    private void SelectNextAnimal()
+    {
+
+
+        selectAnimal(counter);
+        button.enabled = false;
+        counter++;
+
+
+    }
+
+    IEnumerator showDonkey()
+    {
+        shownAnimal.SetActive(false);
+        shownAnimal = donkey;
+        yield return new WaitForSeconds(1f);
+        shownAnimal.SetActive(true);
+        button.enabled = true;
+        timer.Start();
+    }
+
+    IEnumerator showChicken()
+    {
+        shownAnimal.SetActive(false);
+        shownAnimal = chicken;
+        yield return new WaitForSeconds(1f);
+        shownAnimal.SetActive(true);
+        button.enabled = true;
+        timer.Start();
+    }
+
+    IEnumerator showCow()
+    {
+        shownAnimal.SetActive(false);
+        shownAnimal = cow;
+        yield return new WaitForSeconds(1f);
+        shownAnimal.SetActive(true);
+        button.enabled = true;
+        timer.Start();
+    }
+
+    IEnumerator showPig()
+    {
+        shownAnimal.SetActive(false);
+        shownAnimal = pig;
+        yield return new WaitForSeconds(1f);
+        shownAnimal.SetActive(true);
+        button.enabled = true;
+        timer.Start();
+    }
+}
